6784465e7503d2e7f95b3c2e279101552c38d352
Halima192 <74451691+Halima192@users.noreply.github.com>
2021-03-29 12:40:03 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,16 @@
+var drop1;
+
+function preload(){
+    
+}
+
+function setup(){
+    var canvas = createCanvas(400,1200);
+    drop1=new drop(200,200,20,20)
+}
+
+function draw(){
+    
+    drop1.display();
+}   
+
